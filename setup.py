72c2d5fa4dddedfcf2ef71c1f2e7bc3fc2f65a19
Dobri Danchev <12420863+danchev@users.noreply.github.com>
2021-11-03 12:03:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import find_packages, setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name='fake-awsglue',
-    version='0.0.0.post20211019',
-    long_description=__doc__,
+    name='awsglue-dev',
+    version='0.0.0.post20211102',
+    description='Python interfaces to the AWS Glue ETL library for use as a local dependency.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
