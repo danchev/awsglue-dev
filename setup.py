f62f8576ace723b79fd23ded50ba213fca4f14e6
danchev <12420863+danchev@users.noreply.github.com>
2021-12-29 22:07:45 -0600

    Fix versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='awsglue-dev',
-    version='0.0.0.post20211102',
+    version='2021.12.30',
     description='Python interfaces to the AWS Glue ETL library for use as a local dependency.',
     long_description=long_description,
     long_description_content_type='text/markdown',
