a65dea37f1d4f4dcc1c73e087390a3b5bacd6fc2
danchev <12420863+danchev@users.noreply.github.com>
2022-01-04 21:43:55 -0600

    Add twine to dev requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import find_packages, setup
 
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='awsglue-dev',
-    version='2021.12.30',
-    description='Python interfaces to the AWS Glue ETL library for use as a local dependency.',
+    name="awsglue-dev",
+    version="2021.12.30",
+    description="Python interfaces to the AWS Glue ETL library for use as a local dependency.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    extras_require={
+        "dev": [
+            "twine",
+        ]
+    },
 )
